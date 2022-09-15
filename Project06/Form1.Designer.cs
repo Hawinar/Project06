2f3fe45a6b97146f22b16e39a50e178f9d1421a6
Hawinar <egorchelnokov@bk.ru>
2022-09-15 20:11:37 +0300

    Добавьте файлы проекта.

---
+++ Project06/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Project06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btFind = new System.Windows.Forms.Button();
+            this.lbAvgNum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(121, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Числа";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(526, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btFind
+            // 
+            this.btFind.Location = new System.Drawing.Point(327, 388);
+            this.btFind.Name = "btFind";
+            this.btFind.Size = new System.Drawing.Size(109, 50);
+            this.btFind.TabIndex = 1;
+            this.btFind.Text = "Найти";
+            this.btFind.UseVisualStyleBackColor = true;
+            this.btFind.Click += new System.EventHandler(this.btFind_Click);
+            // 
+            // lbAvgNum
+            // 
+            this.lbAvgNum.AutoSize = true;
+            this.lbAvgNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbAvgNum.Location = new System.Drawing.Point(195, 346);
+            this.lbAvgNum.Name = "lbAvgNum";
+            this.lbAvgNum.Size = new System.Drawing.Size(272, 39);
+            this.lbAvgNum.TabIndex = 2;
+            this.lbAvgNum.Text = "Среднее число:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbAvgNum);
+            this.Controls.Add(this.btFind);
+            this.Controls.Add(this.chart1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btFind;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label lbAvgNum;
+    }
+}
+
---
+++ Project06/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Project06
+{
+    /// <summary>
+    /// 3.2 Лабораторная работа № 2. Тема: «Стиль программирования» Вариант № 22 https://github.com/Hawinar
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btFind_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            int[] numbers = new int[20];
+            int[] intervals = new int[10];
+            Random rnd = new Random();
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(0, 11);
+            }
+
+            for(int j = 0; j < intervals.Length;)
+            {
+                for(int k = 0; k < numbers.Length; k += 2)
+                {
+                    if(numbers[k] <= numbers[k + 1])
+                    {
+                        intervals[j] = numbers[k + 1] - numbers[k];
+                        j++;
+                    }
+                    else
+                    {
+                        intervals[j] = numbers[k] - numbers[k + 1];
+                        j++;
+                    }
+                }
+            }
+            float avgNumber = 0;
+            for(int l = 0; l<intervals.Length; l++)
+            {
+                avgNumber += intervals[l];
+            }
+            avgNumber /= intervals.Length;
+            lbAvgNum.Text = $"Среднее число: {avgNumber}";
+            Array.Sort(intervals);
+            for(int i = 0; i < intervals.Length; i++)
+            {
+                chart1.Series[0].Points.Add(intervals[i]);
+            }
+        }
+    }
+}
